432b928903d373d352bc688e4c83df8141d57131
Xea <0xea@autistici.org>
2023-07-26 18:36:55 +0000

    small corrections

--- ff_fan_control.c
+++ ff_fan_control.c
@@ -5,6 +5,10 @@
 #include <pthread.h>
 #include <unistd.h>
 
+
+int board;
+int PID_fan;
+
 int get_ROC_RK3588S_PC_version()
 {
 	FILE * iv = popen("cat /sys/bus/iio/devices/iio:device0/in_voltage5_raw", 0);
@@ -14,8 +18,9 @@ int get_ROC_RK3588S_PC_version()
 		fclose(iv);
 		return -1;
 	}
-	char volt_str[0x3e8];
-	fgets(volt_str, 0x3e8, iv);
+	char volt_str[1000];
+	fgets(volt_str, 1000, iv);
+	// this read should not return more than 32 bytes
 	const int volt = (int) atof(volt_str);
 	fclose(iv);
 	return volt <= 0x64 && volt >= 0 ? -1 :
@@ -89,12 +94,12 @@ void* roc_rk3588s_pc_fan_thread_daemon(void * arg)
 		// potencially write to some thread shared pointer
 	}
 }
-void PID_init(int s3c, float x0[])
+void PID_init(int PID_fan /* x0 */, float x0[])
 {
 	// 0x3c449ba6 in IEEE-754 0.12f
 	// 0x42400000 in IEEE-754 48.0f
 	// 0x000186a0 in IEEE-754 1.4f
-	switch (s3c) {
+	switch (board) {
 	case 0:
 		x0[0]=2.0f;x0[1]=0.12f;x0[2]=1.0f;x0[3]=48.0f;x0[4]=0.0f;
 		x0[5]=0.0f;x0[6]=0.0f; x0[7]=0.0f;x0[8]=0.0f; x0[9]=1.4f;
@@ -120,9 +125,9 @@ void PID_init(int s3c, float x0[])
 	// also making a function out of the initilization of a float array
 	// seams a bit off
 }
-void fan_init(int s3c)
+int fan_init()
 {
-	switch (s3c) {
+	switch (board) {
 		case 0:
 			//fan_CS_R1_3399JD4_MAIN_init();
 			break;
@@ -140,11 +145,11 @@ void fan_init(int s3c)
 			break;
 	}
 	sleep(2);
-	return;
+	return 2;
 }
-void set_fan_pwm(int s3c, int pwm)
+int set_fan_pwm(int pwm)
 {
-	switch (s3c) {
+	switch (board) {
 		case 0:
 			//set_CS_R1_3399JD4_MAIN_fan_pwm(pwm);
 			break;
@@ -161,11 +166,12 @@ void set_fan_pwm(int s3c, int pwm)
 			//set_ROC_RK3588_PC_fan_pwm(pwm);
 			break;
 	}
+	return 2;
 }
-int main(int argc, char **argv)
+int main(int argc/* 1ch */, char **argv /* str */)
 {
 	if (argc <= 1) {
-		puts("./main CS-R1-3399JD4-MAIN --debug");
+		puts("./main CS-R1-3399JD4-MAIN 50");
 		puts("./main CS-R2-3399JD4-MAIN --debug");
 		puts("./main ROC-RK3588S-PC 50");
 		puts("./main ITX_3588J 50");
@@ -175,16 +181,15 @@ int main(int argc, char **argv)
 		return 0;
 	}
 	
-	int s3c;
 	if (!strcmp("CS_R1-3399JD4-MAIN", argv[1])) {
 		//puts("board CS_R1_3399JD4_MAIN");
-		//s3c = 0;
+		//board = 0;
 	} else if (!strcmp("CS-R2-3399JD4-MAIN", argv[1])) {
 		//puts("board CS_R2_3399JD4_MAIN");
-		//s3c = 1;
+		//board = 1;
 	} else if (!strcmp("ROC-RK3588S-PC", argv[1])) {
 		puts("board ROC-RK3588S-PC");
-		s3c = 2;
+		board = 2;
 		const int RK3588S_V = get_ROC_RK3588S_PC_version();
 		if (RK3588S_V == -1) {
 			puts("can not judge ROC-RK3588S-PC version");
@@ -196,14 +201,15 @@ int main(int argc, char **argv)
 		}
 	} else if (!strcmp("ITX_3588J 50", argv[1])) {
 		//puts("board ITX-3588J");
-		//s3c = 3;
+		//board = 3;
 	}
 	// if you didn't change the model formats
 	// you could have just made the models to strings
 	// and you could have done most of this in one place
+	// it would also be smart to error out if no model is specified
 	float x0[10];
-	PID_init(s3c, x0);
-	fan_init(s3c);
+	PID_init(PID_fan, x0);
+	const int ch1 = fan_init();
 	pthread_t t1, t2, t3, t4, t5, t6;
 	if (argc > 2) {
 		// this is not proper argument parsing
@@ -211,12 +217,12 @@ int main(int argc, char **argv)
 		if (strcmp("--debug", argv[2])) {
 			const int pwm = atoi(argv[2]);
 			// and w0, w0, 0xff
-			set_fan_pwm(s3c, pwm);
+			set_fan_pwm(pwm);
 			// those "threadN create error" messages don't make any sense
 			// and don't give any indication were the issue is
 			// also you could have used 1 or 2 pthreads
 			// you have 6 of which at max 2 are used
-			switch (s3c) {
+			switch (board) {
 			case 0:
 				//if (pthread_create(&t1, NULL, cs_r1_3399jd4_main_fan_thread_daemon, NULL) != 0) {
 				//	puts("thread3 create error");
@@ -255,7 +261,7 @@ int main(int argc, char **argv)
 			while (1) sleep(1);
 		}
 	}
-	switch (s3c) {
+	switch (board) {
 	case 0:
 		//if (pthread_create(&t1, NULL, cs_r1_3399jd4_main_fan_thread_daemon, NULL) != 0) {
 		//	puts("thread3 create error");
@@ -291,7 +297,7 @@ int main(int argc, char **argv)
 		//}
 		//break;
 	}
-	set_fan_pwm(s3c, 0);
+	set_fan_pwm(0);
 	//init_sigaction()
 	//init_time()
 	printf("pwm: %d\n", 0);
